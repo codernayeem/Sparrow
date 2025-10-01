26279d0869fafd00ca8206eb794ab8a034b51cb8
Himel-032 <139494430+Himel-032@users.noreply.github.com>
2025-10-01 22:51:30 +0600

    getFollowingPosts controllert function added

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -227,3 +227,28 @@ export const getAllPosts = async (req, res) => {
   }
 };
 
+export const getFollowingPosts = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const following = user.following;
+
+    const feedPosts = await Post.find({ user: { $in: following } })
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "user",
+        select: "-password",
+      })
+      .populate({
+        path: "comments.user",
+        select: "-password",
+      });
+    res.status(200).json(feedPosts);
+  } catch (error) {
+    console.log("Error in get following posts controller:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
