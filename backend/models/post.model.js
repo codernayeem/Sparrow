31718111c1a236f97cee2ee5180b9fc545f1a8e6
Md Nayeem <codernayeem898@gmail.com>
2025-10-04 14:57:39 +0600

    Add reply and mentions support to comments in post schema

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -35,6 +35,40 @@ const postSchema = new mongoose.Schema(
           ref: "User",
           required: true,
         },
+        mentions: [{
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        }],
+        replyTo: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        parentComment: {
+          type: mongoose.Schema.Types.ObjectId,
+        },
+        replies: [{
+          text: {
+            type: String,
+            required: true,
+          },
+          user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+          },
+          mentions: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+          }],
+          replyTo: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+          },
+          createdAt: {
+            type: Date,
+            default: Date.now,
+          },
+        }],
       },
     ],
   },
